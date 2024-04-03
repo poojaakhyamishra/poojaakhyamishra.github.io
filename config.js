fb5d7ead70cba784c3ad8ef904746671a6e5aee1
Akhya (Pooja) Mishra <92906037+pooja0187@users.noreply.github.com>
2024-04-03 23:27:20 +0530

    Pushnow

--- config.js
+++ config.js
@@ -1,1 +1,19 @@
-IDRViewer.config = {"pagecount":2,"title":"CONTACT","author":"Pooja Mishra","subject":"","keywords":"DAGAVLEpwrw,BAGAVIzoEmQ","creator":"Canva","producer":"Canva","creationdate":"D:20240323143849+00'00'","moddate":"D:20240323143849+00'00'","trapped":"","fileName":"PoojaMishraResume.pdf","bounds":[[909,1286],[909,1286]],"bookmarks":[],"thumbnailType":"jpg","pageType":"html","pageLabels":[]};
+IDRViewer.config = {
+    "pagecount": 2,
+    "title": "CONTACT",
+    "author": "Pooja Mishra",
+    "subject": "",
+    "keywords": "DAGAVLEpwrw,BAGAVIzoEmQ",
+    "creator": "Canva",
+    "producer": "Canva",
+    "creationdate": "D:20240323143849+00'00'",
+    "moddate": "D:20240323143849+00'00'",
+    "trapped": "",
+    "fileName": "PoojaMishraResume.pdf",
+    "bounds": [[909, 1286], [909, 1286]],
+    "bookmarks": [],
+    "thumbnailType": "jpg",
+    "pageType": "html"
+    // Remove the pageLabels property here
+  };
+  
